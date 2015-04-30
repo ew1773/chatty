ad020ff0a90a194fc00bb2f7ed9d2c6d3816ead3
Edgar <ew1773@gmail.com>
2015-04-30 15:54:07 -0600

    Step two done

---
+++ server.js
@@ -0,0 +1,24 @@
+var messages = ["this sucks", "just a little"]
+
+var onRequest = function(req, res) {
+	  if (req.method == 'POST') {
+       var postData = '';
+       req.on('data', function(chunk) {
+           postData += chunk.toString();
+        });    
+        req.on('end', function() {
+            console.log("Got POST data:");
+//            console.log(JSON.parse(postData));
+		        messages.push(postData);
+       });
+	   res.end(JSON.stringify(messages));
+	  }else if (req.method === 'GET')
+	res.writeHead(200, {
+		'Connection': 'close',
+		'Content-Type': 'application/json'
+	});
+	res.end(JSON.stringify(messages));
+};
+
+var http = require('http')
+http.createServer(onRequest).listen(8081)
